5312e8ec3625a5cbff9a9b83e7864eac5bb071a2
unknown <massaro@mail.bg>
2018-01-25 15:27:59 +0200

    added test project

---
+++ src/app/app.module.ts
@@ -0,0 +1,27 @@
+
+import { AppRoutingModule } from './app-routing.module';
+
+// Angular
+import { RouterModule } from '@angular/router';
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { ReactiveFormsModule } from '@angular/forms';
+import { HttpClientModule } from '@angular/common/http';
+import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
+
+// Components
+import { AppComponent } from './app.component';
+
+@NgModule({
+  declarations: [
+    AppComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    RouterModule
+  ],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
---
+++ src/environments/environment.prod.ts
@@ -0,0 +1,5 @@
+export const environment = {
+  production: true,
+  environmentType: 'prod',
+  baseUrl: ''
+};
